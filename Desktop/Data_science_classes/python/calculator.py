7b06b4dcad635e7eed5eba8f295c46f4cbff74b0
sangameshwarans <sangameshsmartr@gmail.com>
2025-09-24 21:27:41 +0530

    version2

--- Desktop/Data_science_classes/python/calculator.py
+++ Desktop/Data_science_classes/python/calculator.py
@@ -1,2 +1,3 @@
 print("addition: ", 2+2)
 print("subtraction: ", 5-3)
+print("Multiplication: ", 3*5)
---
+++ Desktop/Data_science_classes/python/python_learning.py
@@ -0,0 +1,12 @@
+x='1'
+y='1'
+print(x+y)
+
+f=22.2
+print(f, type(f))
+
+is_active=True
+print(is_active, type(is_active))
+
+address = None
+print(address, type(address))
